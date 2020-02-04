8345302aa0f29350ab001664af9ae1ba537ad0ef
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-03 19:56:45 -0700

    Add assert for health and attribute method

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -15,17 +15,12 @@ class ShipTest < Minitest::Test
 
     assert_equal "Cruiser", cruiser.name
     assert_equal 3, cruiser.length
+    assert_equal 3, cruiser.health
   end
-
-
 
 
 end
 
-
-# pry(main)> cruiser.length
-# #=> 3
-#
 # pry(main)> cruiser.health
 # #=> 3
 #
