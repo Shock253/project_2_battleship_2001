561a22878cb6d56beeb568c28fac5d5194bf6769
Ethan Hocking <eshocking253@gmail.com>
2020-02-04 13:28:14 -0700

    Add fired_upon? method and test

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,6 +4,7 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def empty?
@@ -14,4 +15,7 @@ class Cell
     @ship = ship
   end
 
+  def fired_upon?
+    @fired_upon
+  end
 end
