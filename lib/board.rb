7f0982387fd30b2eb60062ab424a8f4b9947e7cb
Ethan Hocking <eshocking253@gmail.com>
2020-02-04 15:50:19 -0700

    Add test - test_has_cells and cells initialization

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,13 @@
 class Board
-
+  attr_reader :cells
   def initialize
-    
-  end
+    @cells = Hash.new
+    letters = "A".."D"
 
+    letters.to_a.each do |letter|
+      4.times do |number|
+        @cells.merge!("#{letter}#{number + 1}" => Cell.new("#{letter}#{number + 1}"))
+      end
+    end
+  end
 end
