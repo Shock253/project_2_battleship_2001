76120dc4b3a95ccbab329533641f19c7728729d0
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-05 15:49:02 -0700

    Add setup method to board_test

--- test/board_test.rb
+++ test/board_test.rb
@@ -3,32 +3,43 @@ require "minitest/pride"
 require "./lib/board"
 require "./lib/cell"
 require "./lib/ship"
-require "pry"
 
 class ClassTest < Minitest::Test
-  def test_it_exists
-    board = Board.new
 
-    assert_instance_of Board, board
+  def setup
+
+    @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    @titanic = Ship.new("Titanic", 3)
+    @titanic.hit
+    @titanic.hit
+    @titanic.hit
+
+  end
+
+  def test_it_exists
+
+    assert_instance_of Board, @board
   end
 
   def test_has_cells
-    board = Board.new
 
-    assert_instance_of Hash, board.cells
-    assert_equal 16, board.cells.size
-    board.cells.each do |cell|
+    assert_instance_of Hash, @board.cells
+    assert_equal 16, @board.cells.size
+    @board.cells.each do |cell|
       assert_instance_of Cell, cell[1]
     end
   end
 
   def test_coordinate_validation
-    board = Board.new
 
-    assert_equal true, board.valid_coordinate?("A1")
-    assert_equal true, board.valid_coordinate?("D4")
-    assert_equal false, board.valid_coordinate?("A5")
-    assert_equal false, board.valid_coordinate?("E1")
-    assert_equal false, board.valid_coordinate?("A22")
+    assert_equal true, @board.valid_coordinate?("A1")
+    assert_equal true, @board.valid_coordinate?("D4")
+    assert_equal false, @board.valid_coordinate?("A5")
+    assert_equal false, @board.valid_coordinate?("E1")
+    assert_equal false, @board.valid_coordinate?("A22")
   end
+
+  
 end
