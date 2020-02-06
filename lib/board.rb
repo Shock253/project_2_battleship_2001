765b80154170708f717934fe13d8dc1fe81ac80b
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-06 08:44:38 -0700

    WIP: Add test valid_placement? method and test

--- lib/board.rb
+++ lib/board.rb
@@ -18,4 +18,19 @@ class Board
     number = coordinate[1].to_i
     valid_letters.include?(coordinate[0]) && valid_numbers.include?(number) && coordinate.length == 2
   end
+
+  def valid_placement?(ship, ship_coordinates)
+    if ship.length == ship_coordinates.size
+      is_consecutive_letters = false
+      is_consecutive_numbers = false
+
+      # first_number = ship_coordinates[0][1].to_i
+      ship_coordinates.each do |coordinate|
+        
+      end
+
+    else
+      false
+    end
+  end
 end
