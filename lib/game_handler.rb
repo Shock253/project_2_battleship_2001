a8a8562da658806548940387f36f24b084bff1d9
Ethan Hocking <eshocking253@gmail.com>
2020-02-07 11:39:42 -0700

    Add game_handler skeleton

---
+++ lib/game_handler.rb
@@ -0,0 +1,28 @@
+
+class GameHandler
+
+  def initialize
+    @user_board = Board.new
+    @computer_board = Board.new
+
+    @user_ships = []
+    @computer_ships = []
+  end
+
+
+  def setup_game
+
+  end
+
+  def debug_win_conditions
+
+  end
+
+  def take_turn
+
+  end
+
+  def start_game
+
+  end
+end
